9b8757c78ae63a1defe5f74685f140089d81f90e
Lucas Apparecido de Oliveira <108204218+LucasApparecido@users.noreply.github.com>
2023-10-03 10:38:51 -0300

    Separação das atividades

---
+++ SQL/Agencia_Empregos_Exercicios_JP.sql
@@ -0,0 +1,8 @@
+--1. Listar os dados dos Trabalhadores;
+
+
+--3. Listar os dados da entrevista: nome do solicitante, nome da vaga, 
+--   descrição da vaga, data da entrevista, nome do entrevistado e resultado final da entrevista;
+
+
+--5. Total de vagas da agencia;
---
+++ SQL/Agencia_Empregos_Exercicios_Lucas.sql
@@ -0,0 +1,9 @@
+--2. Listar o Solicitantes da cidade de Anápolis;
+
+
+--4. Listar as Vagas que já tiveram entrevista 
+--   (nome do solicitante, e descrição da vaga e salário da vaga)
+
+
+--6. Total de trabalhadores entrevistados para a vaga 
+--   (Descrição da vaga, total de entrevistados);
